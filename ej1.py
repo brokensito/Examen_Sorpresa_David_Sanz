837aab531675e2257494474bbb82c796bdda0ceb
brokensito <dsanzfuertes.15@gmail.com>
2022-09-24 00:30:33 +0200

    Hola.py -> Generador, string y cuadrante

---
+++ ej1.py
@@ -0,0 +1,20 @@
+
+
+
+numero1 = int(input("Escribe el primer numero:"))
+
+numero2 = int(input("Escribe el segundo numero:"))
+
+
+class Punto:
+
+
+    def __init__(self):
+        print("Hola")
+      
+punto = Punto()
+
+
+        
+
+
