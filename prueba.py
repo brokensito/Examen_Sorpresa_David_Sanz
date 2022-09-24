ed793c0f017f314c04dc482e097e30984200bc15
David Sanz <dsanzfuertes.15@gmail.com>
2022-09-24 20:43:45 +0200

    Guardar cambios

--- prueba.py
+++ prueba.py
@@ -1,18 +1,6 @@
 
 
 
-numero1 = int(input("Escribe el primer numero:"))
-
-numero2 = int(input("Escribe el segundo numero:"))
-
-
-class Punto:
-
-
-    def __init__(self):
-        print("Hola")
-      
-punto = Punto()
 
 
         
